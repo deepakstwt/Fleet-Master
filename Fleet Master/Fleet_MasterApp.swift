8f6e4fd5ac7ea135ddab72145ddd6d483e06f39a
Kushagra Kulshrestha <140870916+Ricky30feb@users.noreply.github.com>
2025-03-18 11:44:18 +0530

    Initial Commit

---
+++ Fleet Master/Fleet_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fleet_MasterApp.swift
+//  Fleet Master
+//
+//  Created by Kushagra Kulshrestha on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Fleet_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fleet MasterTests/Fleet_MasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fleet_MasterTests.swift
+//  Fleet MasterTests
+//
+//  Created by Kushagra Kulshrestha on 18/03/25.
+//
+
+import Testing
+@testable import Fleet_Master
+
+struct Fleet_MasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
